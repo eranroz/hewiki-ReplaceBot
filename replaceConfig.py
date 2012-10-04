abca3bd542b90619dcf600114283bddfa167c72e
eranroz <eranroz89@gmail.com>
2012-10-04 21:56:55 +0200

    First version

---
+++ replaceConfig.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8  -*-
+replacementsPage=u'ויקיפדיה:בוט/בוט החלפות/רשימת החלפות נוכחית'
+defaultSummary=u'בוט החלפות: '
+safeTemplates=[u'ציטוט',u'ציטוטון',u'חלונית',u'מסגרת',u'הדגשה']
+safeTemplatesCategories=[u'תבניות קישורים חיצוניים', u'תבניות ציטוט']
+nobotRgx=u"\{\{ללא בוט\|([0-9]+)\}\}"
+fileUsageRgx=u'\[\[(File|Image|תמונה|קובץ)\s*:\s*.*?[\|\]]'
+redirectRgx=u'#\s*(הפניה|REDIRECT)\s*\[\['
+namespaces=[0,14,100]
