4695ddbf4ec7d3995c7bef2d67c6580f16f8e133
eranroz <eranroz89@gmail.com>
2012-12-28 00:37:17 +0200

    Adding template to safe templates

--- replaceConfig.py
+++ replaceConfig.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8  -*-
 replacementsPage=u'ויקיפדיה:בוט/בוט החלפות/רשימת החלפות נוכחית'
 defaultSummary=u'בוט החלפות: '
-safeTemplates=[u'ציטוט',u'ציטוטון',u'חלונית',u'מסגרת',u'הדגשה']
+safeTemplates=[u'ציטוט',u'ציטוטון',u'חלונית',u'מסגרת',u'הדגשה',u'קמץ קטן']
 safeTemplatesCategories=[u'תבניות קישורים חיצוניים', u'תבניות ציטוט']
 nobotRgx=u"\{\{ללא בוט\|([0-9]+)\}\}"
 fileUsageRgx=u'\[\[(File|Image|תמונה|קובץ)\s*:\s*.*?[\|\]]'
